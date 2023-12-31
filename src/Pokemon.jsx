a49b0415235bd3829ef4edbdcd3b798bbb856926
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2023-12-31 14:00:14 -0600

    organized code

--- src/Pokemon.jsx
+++ src/Pokemon.jsx
@@ -3,14 +3,21 @@ import useFetchPokemonAPIData from './FetchAPIData';
 const DisplayPokemon = () => {
     const [cardArray, setCardArray] = useState([]);
     const {setData, fetchData, data} = useFetchPokemonAPIData();
-    let i = data.length;
-    while (--i > 0) {
+    
+    const randomizeCardsOnClick = () => {
+      let i = data.length;
+      while (--i > 0) {
       let temp = Math.floor(Math.random() * (i + 1));
             [data[temp], data[i]] = [data[i], data[temp]];
     }
-    //to rerender onclick
+    }
+
+    //end the gameonclick if there's a match
+    //click and dont match, i++ score
+    //style
 
     const checkForMatches = (id) => {
+  
       //check for matches
       const previousCards = [...cardArray]
       const clickedCardId = id;
@@ -21,15 +28,16 @@ const DisplayPokemon = () => {
         if (card === clickedCardId) {
           matching = true;
           console.log('its a match')
-          return
+          return matching = true;
         } 
         
         if (!matching) {
           matching = false;
           console.log('its not a match')
-
+          return matching = false;
         }
       })
+
       let allCardsArray = [...cardArray, id]
       setCardArray(allCardsArray)
 
@@ -48,7 +56,8 @@ const DisplayPokemon = () => {
               src={pokemon.sprites.front_default}
               alt="pokemon-images"
               onClick={() => {
-                checkForMatches(pokemon.id);}}
+                checkForMatches(pokemon.id);
+                randomizeCardsOnClick();}}
             />
             <p>{pokemon.name}</p>
           </div>
